0246e474c0bdfc70f31e03d6167dd659d800cb77
vegarrsm <vegarrsm@stud.ntnu.no>
2021-04-16 19:23:54 +0200

    --amedn

---
+++ client/assets/models.mjs
@@ -0,0 +1,16 @@
+const _CHARACTER_MODELS = {
+    robot: {
+      base: 'Robot.fbx',
+      path: './',
+/*       anchors: {
+        rightHand: 'RightHandIndex1',
+      }, */
+/*       attack: {
+        timing: 0.35,
+        cooldown: 1.0,
+        type: 'melee',
+        range: 10,
+      }, */
+      scale: 6.0,
+      name: 'Robot',
+    }};
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,87 +1,97 @@
-import React, {useState} from 'react';
-import * as th from 'three';
-import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
-import './entities/entity'
-import { Entity } from './entities/entity';
+import React, { useState } from "react";
+import * as th from "three";
+import { Loader } from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import "./entities/entity";
+import { Entity } from "./entities/entity";
+import { StaticModelComponent } from "./components/gltf-component";
+import { GLTFLoader } from "three-gltf-loader";
+import { FBXLoader } from "three-fbx-loader";
 //import './entities/player';
 
 let camera, scene, renderer, cube;
 
 const Main = () => {
-    function init() {
-        // Init scene
-        scene = new th.Scene();
+  function init() {
+    // Init scene
+    scene = new th.Scene();
 
-        // Init camera (PerspectiveCamera)
-        camera = new th.PerspectiveCamera(
-            100,
-            window.innerWidth / window.innerHeight,
-            0.1,
-            1000
-        );
+    // Init camera (PerspectiveCamera)
+    camera = new th.PerspectiveCamera(
+      100,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
 
+    // Init renderer
+    renderer = new th.WebGLRenderer({ antialias: true });
+    const canvas = renderer.domElement;
+    const controls = new OrbitControls(camera, canvas);
+    controls.target.set(0, 5, 0);
+    controls.update();
 
-        // Init renderer
-        renderer = new th.WebGLRenderer({antialias: true});
-        const canvas = renderer.domElement;
-        const controls = new OrbitControls(camera, canvas);
-        controls.target.set(0, 5, 0);
-        controls.update();
+    // Set size (whole window)
+    renderer.setSize(window.innerWidth, window.innerHeight);
 
-        // Set size (whole window)
-        renderer.setSize(window.innerWidth, window.innerHeight);
+    // Render to canvas element
+    document.body.appendChild(canvas);
 
-        // Render to canvas element
-        document.body.appendChild(canvas);
-
-        {// Add sun
-            const color = 0xFFFFFF;
-            const intensity = 1;
-            const light = new th.DirectionalLight(color, intensity);
-            light.position.set(-1, 2, 4);
-            scene.add(light);
-        }
-        {// Add ambient
-            const color = 0xFFFFFF;
-            const intensity = 0.5;
-            const light = new th.AmbientLight(color, intensity);
-            scene.add(light);
-        }
-
-        // Position camera
-        camera.position.set(0, 10, 20);
+    {
+      // Add sun
+      const color = 0xffffff;
+      const intensity = 1;
+      const light = new th.DirectionalLight(color, intensity);
+      light.position.set(-1, 2, 4);
+      scene.add(light);
     }
-
-    // Draw the scene every time the screen is refreshed
-    function animate() {
-        requestAnimationFrame(animate);
-
-        // Add animation here
-
-        renderer.render(scene, camera);
+    {
+      // Add ambient
+      const color = 0xffffff;
+      const intensity = 0.5;
+      const light = new th.AmbientLight(color, intensity);
+      scene.add(light);
     }
+    const loader = new FBXLoader();
+    loader.setPath("../assets/");
+    loader.load("Robot.fbx", (fbx) => {
+      this._OnLoaded(fbx);
+    });
 
-    function onWindowResize() {
-        // Camera frustum aspect ratio
-        camera.aspect = window.innerWidth / window.innerHeight;
-        // After making changes to aspect
-        camera.updateProjectionMatrix();
-        // Reset size
-        renderer.setSize(window.innerWidth, window.innerHeight);
-    }
+    // Position camera
+    camera.position.set(0, 10, 20);
+  }
 
-    window.addEventListener('resize', onWindowResize, false);
+  // Draw the scene every time the screen is refreshed
+  function animate() {
+    requestAnimationFrame(animate);
 
-    const [first, setfirst] = useState(true);
-    if (first) {
-        init();
-        animate();
-        setfirst(false);
-    }
+    // Add animation here
 
-    // Entities
+    renderer.render(scene, camera);
+  }
 
-    return <div/>;
-}
+  function onWindowResize() {
+    // Camera frustum aspect ratio
+    camera.aspect = window.innerWidth / window.innerHeight;
+    // After making changes to aspect
+    camera.updateProjectionMatrix();
+    // Reset size
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+
+  window.addEventListener("resize", onWindowResize, false);
+
+  const [first, setfirst] = useState(true);
+  if (first) {
+    init();
+    animate();
+    setfirst(false);
+  }
+
+  // Entities
+
+  return <div />;
+};
 
 export default Main;
