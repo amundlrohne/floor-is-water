7113473d760d4d495524d5c60e52382e6545f1a4
Jørgen Nummedal Sveberg <jorgenns@stud.ntnu.no>
2021-04-16 15:04:07 +0200

    made basic game world with some lighting, plane and walls

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,39 @@
 import React from 'react'
+import GameWorld from "./GameWorld.jsx";
+import Home from "./Home.js";
+import './App.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 const App = () => {
-  return <div style={{ display: 'flex', justifyContent: 'center' }}>Hello from React</div>
+  return (
+      <Router>
+          <div>
+              <nav>
+                  <ul>
+                      <li>
+                          <Link to="/">Home</Link>
+                      </li>
+                      <li>
+                          <Link to="/gameWorld">Game</Link>
+                      </li>
+                  </ul>
+              </nav>
+              <Switch>
+                  <Route path="/gameWorld">
+                      <GameWorld />
+                  </Route>
+                  <Route path="/">
+                      <Home />
+                  </Route>
+              </Switch>
+          </div>
+      </Router>
+  )
 }
 
 export default App
