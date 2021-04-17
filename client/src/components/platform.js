94bfa8365273b217ecd0846f9270775859392b72
Jørgen Nummedal Sveberg <jorgenns@stud.ntnu.no>
2021-04-17 12:18:59 +0200

    added platforms with textures and fileloader

--- client/src/components/platform.js
+++ client/src/components/platform.js
@@ -1,26 +1,42 @@
 import * as three from 'three';
 import Component from './component';
-export default class Platform extends Component {
+import woodImg from '../Assets/WoodenPlatform.jpg';
+import stoneImg from '../Assets/StonePlatform.jpg';
+export class PlatformColumn extends Component {
 
     constructor(params) {
         super();
-        this.InitComponent(params);
         this.params = params;
         if (params) {
             const platformGeo = new three.CylinderGeometry(params.radius, params.radius, params.height, params.segments);
-            const platformMat = new three.MeshPhongMaterial({color: 0x00ff00});
+            const texture = new three.TextureLoader().load(stoneImg);
+            const platformMat = new three.MeshPhongMaterial({map: texture});
             this.mesh = new three.Mesh(platformGeo, platformMat);
             params.scene.add(this.mesh);
         }
     }
 
-    SetParent(p) {
-        super.SetParent(p);
-        const position = this.Parent.Position;
-        console.log(position.y);
-        console.log(this.params.height);
-        console.log(this.params.height/2);
-        console.log(position.y+(this.params.height/2));
-        this.mesh.position.set(position.x, position.y+(this.params.height/2), position.z);
+    UpdatePosition(p) {
+        console.log(p);
+        this.mesh.position.copy(p);
+    }
+}
+export class PlatformFloating extends Component {
+
+    constructor(params) {
+        super();
+        this.params = params;
+        if (params) {
+            const platformGeo = new three.BoxGeometry(params.width, params.height, params.depth, params.segments, params.segments, params.segments);
+            const texture = new three.TextureLoader().load(woodImg);
+            const platformMat = new three.MeshPhongMaterial({map: texture});
+            this.mesh = new three.Mesh(platformGeo, platformMat);
+            params.scene.add(this.mesh);
+        }
+    }
+
+    UpdatePosition(p) {
+        console.log(p);
+        this.mesh.position.copy(p);
     }
 }
