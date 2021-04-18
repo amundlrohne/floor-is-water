8f82cecf5f5de7152ce080d3af87ff839d0400e4
Sarunas Juskenas <sarunas02@gmail.com>
2021-04-18 12:55:28 +0200

    #2 different model based on powerup type

--- client/src/components/active-power-up.js
+++ client/src/components/active-power-up.js
@@ -3,6 +3,10 @@ import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
 import pallete from "../assets/PalleteTex.png";
 import * as three from "three";
 
+import gem4 from "../assets/gem4.fbx";
+import gem1 from "../assets/gem1.fbx";
+import gem7 from "../assets/gem7.fbx";
+
 export default class Powerup extends Component {
   constructor(params) {
     super();
@@ -10,27 +14,46 @@ export default class Powerup extends Component {
     this.params = params;
     if (params) {
       const loader = new FBXLoader();
-      loader.load(params.model, function (result) {
-        result.position.copy(params.position);
 
-        let map = new three.TextureLoader().load(pallete);
-        map.encoding = three.sRGBEncoding;
-
-        let mesh = result.children[0];
-        mesh.material = new three.MeshPhongMaterial({
-          map: map,
-          color: 0xff00ff,
-        });
-
-        mesh.scale.setScalar(5);
-        mesh.traverse((c) => {
-          c.castShadow = true;
-        });
-
-        mesh.position.copy(params.position);
-
-        params.scene.add(mesh);
-      });
+      switch (params.type) {
+        case "speed":
+          loader.load(gem1, function (result) {
+            loadModel(result, params);
+          });
+          break;
+        case "shield":
+          loader.load(gem7, function (result) {
+            loadModel(result, params);
+          });
+          break;
+        case "punch":
+          loader.load(gem4, function (result) {
+            loadModel(result, params);
+          });
+          break;
+      }
     }
   }
+}
+
+function loadModel(result, params) {
+  result.position.copy(params.position);
+
+  let map = new three.TextureLoader().load(pallete);
+  map.encoding = three.sRGBEncoding;
+
+  let mesh = result.children[0];
+  mesh.material = new three.MeshPhongMaterial({
+    map: map,
+    // color: 0xff00ff,
+  });
+
+  mesh.scale.setScalar(5);
+  mesh.traverse((c) => {
+    c.castShadow = true;
+  });
+
+  mesh.position.copy(params.position);
+
+  params.scene.add(mesh);
 }
