b58f35b2d4222f16ee9d6ecb5c1f7e92a6573479
vegarrsm <vegarrsm@stud.ntnu.no>
2021-04-23 11:23:28 +0200

    implemented player movement

--- client/src/components/player-input.js
+++ client/src/components/player-input.js
@@ -6,7 +6,15 @@ import { Vector3 } from "three";
 export class PlayerInput extends Component {
     constructor(params) {
         super();
-        this.keysPressed = { w: false, a: false, s: false, d: false };
+        this.run = [0, 0];
+        this.keysPressed = {
+            w: false,
+            a: false,
+            s: false,
+            d: false,
+            Space: false,
+        };
+        this.jumpReady = true;
         this.Init();
     }
 
@@ -19,6 +27,50 @@ export class PlayerInput extends Component {
         //MUST BE SWITCHED WITH TOUCH UI ELEMENTS
         //console.log(this.Parent.params.physicsHandler)
 
+        /* document.addEventListener("keydown", (event) => {
+            if (
+                event.key == "w" ||
+                event.key == "a" ||
+                event.key == "s" ||
+                event.key == "d" ||
+                event.key == "p"
+            )
+                this.keysPressed[event.key] = true;
+            if (event.code == "Space") {
+                this.keysPressed[event.code] = true;
+            }
+        }); */
+
+        document.addEventListener("keyup", (event) => {
+            if (event.code == "Space") {
+                this.keysPressed[event.code] = false;
+            } else {
+                this.keysPressed[event.key] = false;
+            }
+
+            if (event.key == "w") {
+                this.run[1] = 0;
+            }
+            if (event.key == "s") {
+                this.run[1] = 0;
+            }
+            if (event.key == "a") {
+                this.run[0] = 0;
+            }
+            if (event.key == "d") {
+                this.run[0] = 0;
+            }
+            this.Parent.params.physicsHandler.stopVelocity(
+                "player",
+                this.run[0],
+                this.run[1]
+            );
+            this.Parent.params.physicsHandler.accelerate(
+                this.run[0],
+                this.run[1]
+            );
+            this.keyDown(event);
+        });
         document.addEventListener("keydown", (event) => {
             if (
                 event.key == "w" ||
@@ -29,112 +81,37 @@ export class PlayerInput extends Component {
             )
                 this.keysPressed[event.key] = true;
             if (event.code == "Space") {
-                console.log("SPACE");
-                this.keysPressed[event.code] == true;
+                this.keysPressed[event.code] = true;
             }
-        });
-
-        document.addEventListener("keyup", (event) => {
-            let num = 0;
-/*             Object.keys(this.keysPressed).forEach((e) => {
-                if (this.keysPressed[e] == true) {
-                    num += 1;
-                    console.log(e)
-                }
-            });
-            console.log(event.key + "HER");
-            if(event.code!="Space"){
-            this.keysPressed[event.key] = false;}else{this.keysPressed[event.code] = false;}
-            if ((num = 1)) {
-                console.log("STOP");
-            } */
-            if(event.code!="Space"){
-                this.keysPressed[event.key] = false;}else{this.keysPressed[event.code] = false;}
-            num = 0;
-            /* if (event.key == "w"||event.key == "s") {
-                console.log("STOPWS")
-                this.Parent.params.physicsHandler.applyVelocity(
-                    "player",
-                    new Vector3(0, 0, -1*(this.Parent.params.physicsHandler.findObject("player").velocity.z+10))
-                );
-            } */
-            if (event.key == "a"||event.key =="d"||event.key == "w"||event.key == "s") {
-                console.log("STOPAD")
-                this.Parent.params.physicsHandler.stopVelocity("player")
-                setTimeout(() => { this.Parent.params.physicsHandler.extraStopVelocity("player") }, 50);
-                setTimeout(() => { this.Parent.params.physicsHandler.extraStopVelocity("player") }, 100);
-                /* this.Parent.params.physicsHandler.applyVelocity(
-                    "player",
-                    new Vector3(-1*(this.Parent.params.physicsHandler.findObject("player").velocity.x+10), 0, 0)
-                ); */
-            }
-            console.log("STOP")
-        });
-        document.addEventListener("keydown", (e) => {
-            console.log(this.keysPressed);
-            if (this.keysPressed["w"] == true) {
-                if (
-                    this.Parent.params.physicsHandler.findObject("player")
-                        .velocity.z > -10
-                ) {
-                    this.Parent.params.physicsHandler.applyVelocity(
-                        "player",
-                        new Vector3(0, 0, -10)
-                    );
-                }
-            }
-            if (this.keysPressed["a"] == true) {
-                if (
-                    this.Parent.params.physicsHandler.findObject("player")
-                        .velocity.x > -10
-                ) {
-                    this.Parent.params.physicsHandler.applyVelocity(
-                        "player",
-                        new Vector3(-10, 0, 0)
-                    );
-                }
-            }
-            if (this.keysPressed["s"] == true) {
-                if (
-                    this.Parent.params.physicsHandler.findObject("player")
-                        .velocity.z < 10
-                ) {
-                    this.Parent.params.physicsHandler.applyVelocity(
-                        "player",
-                        new Vector3(0, 0, 10)
-                    );
-                }
-            }
-            if (this.keysPressed["d"] == true) {
-                if (
-                    this.Parent.params.physicsHandler.findObject("player")
-                        .velocity.x < 10
-                ) {
-                    this.Parent.params.physicsHandler.applyVelocity(
-                        "player",
-                        new Vector3(10, 0, 0)
-                    );
-                }
-            }
-            if (this.keysPressed["Space"] == true) {
-                if (
-                    this.Parent.params.physicsHandler.findObject("player")
-                        .velocity.y < 0.1
-                ) {
-                    this.Parent.params.physicsHandler.applyVelocity(
-                        "player",
-                        new Vector3(0, 50, 0)
-                    );
-                }
-            }
-            if (this.keysPressed["p"] == true) {
-                if (
-                    this.Parent.params.physicsHandler.findObject("player")
-                        .velocity.y < 0.1
-                ) {
-                    this.Parent.params.physicsHandler.stopVelocity("player");
-                }
-            }
+            this.keyDown(event);
         });
     }
+    keyDown(e){
+        
+        if (this.keysPressed["w"]) {
+            this.run[1] = -2;
+        }
+        if (this.keysPressed["s"]) {
+            this.run[1] = 2;
+        }
+        if (this.keysPressed["a"]) {
+            this.run[0] = -2;
+        }
+        if (this.keysPressed["d"]) {
+            this.run[0] = 2;
+        }
+        this.Parent.params.physicsHandler.accelerate(
+            this.run[0],
+            this.run[1]
+        );
+
+        if (this.keysPressed["Space"] == true) {
+            {
+                this.Parent.params.physicsHandler.playerJump(
+                    "player",
+                );
+                this.Parent.params.entitySystem.Get("player")._rotation.y=50;
+            }
+        }
+    };
 }
